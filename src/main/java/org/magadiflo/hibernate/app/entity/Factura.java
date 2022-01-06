dc1c851f7fe311c5585729bfd897a3a93df6255d
Martín <magadiflo@gmail.com>
2022-01-06 18:47:19 -0500

    Creando nuevo proyecto y la relación ManyToOne

---
+++ src/main/java/org/magadiflo/hibernate/app/entity/Factura.java
@@ -0,0 +1,69 @@
+package org.magadiflo.hibernate.app.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "facturas")
+public class Factura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descripcion;
+    private Long total;
+
+    @ManyToOne
+    private Cliente cliente;
+
+    public Factura() {
+    }
+
+    public Factura(String descripcion, Long total) {
+        this.descripcion = descripcion;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Factura{");
+        sb.append("id=").append(id);
+        sb.append(", descripcion='").append(descripcion).append('\'');
+        sb.append(", total=").append(total);
+        sb.append(", cliente=").append(cliente);
+        sb.append('}');
+        return sb.toString();
+    }
+}
